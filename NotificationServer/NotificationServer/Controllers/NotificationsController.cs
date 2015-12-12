e440ca09c25252f7bf5f4704c43677dcdccedfbc
Brent Villanueva <bvillanueva@mdsol.com>
2015-12-13 00:30:58 +0900

    apply groups

--- NotificationServer/NotificationServer/Controllers/NotificationsController.cs
+++ NotificationServer/NotificationServer/Controllers/NotificationsController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.AspNet.SignalR;
 using NotificationServer.Models;
@@ -16,26 +17,44 @@ namespace NotificationServer.Controllers
             statusHub = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
         }
 
-        // 1. broadcast to all
-        [Route("api/broadcast")]
+        // register to a group
+        [Route("api/group/register")]
         [HttpPost]
-        public HttpResponseMessage Broadcast([FromBody] Notification notification)
+        public async Task<HttpResponseMessage> Register([FromBody] UserGroupRegistration userRegistration)
         {
-            statusHub.Clients.All.updateStatus(notification);
+            await statusHub.Groups.Add(userRegistration.ClientId, userRegistration.GroupName);
             return Request.CreateResponse(HttpStatusCode.OK);
         }
 
-        // 2. send to particular client from all
+        // send to particular client
         [Route("api/send")]
         [HttpPost]
         public HttpResponseMessage Send([FromBody] NotificationForSpecificUser notificationForSpecificUser)
-        {
+        {   
             var targetClient = statusHub.Clients.Client(notificationForSpecificUser.ClientId);
             if (targetClient == null)
             {
                 return Request.CreateResponse(HttpStatusCode.NotFound);
             }
             statusHub.Clients.Client(notificationForSpecificUser.ClientId).updateStatus(notificationForSpecificUser.Notification);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // broadcast to a group
+        [Route("api/group/broadcast")]
+        [HttpPost]
+        public HttpResponseMessage GroupBroadcast([FromBody] NotificationForGroup notificationForGroup)
+        {
+            statusHub.Clients.Group(notificationForGroup.GroupName).updateStatus(notificationForGroup.Notification);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // broadcast to all
+        [Route("api/broadcast")]
+        [HttpPost]
+        public HttpResponseMessage Broadcast([FromBody] Notification notification)
+        {
+            statusHub.Clients.All.updateStatus(notification);
             return Request.CreateResponse(HttpStatusCode.OK);
         }
     }
